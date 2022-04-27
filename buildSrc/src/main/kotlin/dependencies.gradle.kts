b3d97078851fe3e7ea2d02844f0f5fd27e9137ee
Mina Kleid <minazakiz@gmail.com>
2022-04-27 10:19:00 +0200

    adding kts support

---
+++ buildSrc/src/main/kotlin/dependencies.gradle.kts
@@ -0,0 +1,100 @@
+object Versions {
+    const val navVersion = "2.4.1"
+    const val roomVersion = "2.4.1"
+    const val moshiVersion = "1.13.0"
+    const val retrofitVersion = "2.9.0"
+    const val okhttpVersion = "4.9.3"
+    const val coroutineVersion = "1.4.3"
+}
+
+object Libraries {
+    object App {
+
+        object AndroidX {
+            const val activity = "androidx.activity:activity-ktx:1.2.3"
+            const val annotations = "androidx.annotation:annotation:1.2.0"
+            const val appcompat = "androidx.appcompat:appcompat:1.3.1"
+            const val constraint = "androidx.constraintlayout:constraintlayout:2.0.4"
+            const val core = "androidx.core:core-ktx:1.6.0"
+            const val viewBinding = "androidx.databinding:viewbinding:4.2.0@aar"
+            const val fragment = "androidx.fragment:fragment-ktx:1.6.0"
+            const val material = "com.google.android.material:material:1.5.0"
+            const val recyclerView = "androidx.recyclerview:recyclerview:1.2.0"
+            const val viewPager2 = "androidx.viewpager2:viewpager2:1.0.0"
+        }
+
+        object Hilt {
+            const val core = "com.google.dagger:hilt-android:2.38.1"
+            const val compiler = "com.google.dagger:hilt-android-compiler:2.38.1"
+        }
+
+        object Kotlin {
+            const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:1.6.10"
+        }
+
+        object Coroutines {
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutineVersion}"
+            const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutineVersion}"
+        }
+
+        object Lifecycle {
+
+            const val common = "androidx.lifecycle:lifecycle-common-java8:2.3.1"
+            const val runtime = "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1"
+            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1"
+        }
+
+        object Navigation {
+            const val fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navVersion}"
+            const val ui = "androidx.navigation:navigation-ui-ktx:${Versions.navVersion}"
+            const val dynamicFeatures =
+                "androidx.navigation:navigation-dynamic-features-fragment:${Versions.navVersion}"
+        }
+
+        const val gson = "com.google.code.gson:gson:2.8.8"
+
+        object Room {
+            const val runtime = "androidx.room:room-runtime:${Versions.roomVersion}"
+            const val annotationProcessor = "androidx.room:room-compiler:${Versions.roomVersion}"
+            const val ktx = "androidx.room:room-ktx:${Versions.roomVersion}"
+        }
+
+        object Network {
+            object Retrofit2 {
+                const val converterMoshi =
+                    "com.squareup.retrofit2:converter-moshi:${Versions.retrofitVersion}"
+                const val core = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}"
+            }
+
+            object Moshi {
+                const val adapters = "com.squareup.moshi:moshi-adapters:${Versions.moshiVersion}"
+                const val core = "com.squareup.moshi:moshi:${Versions.moshiVersion}"
+                const val codegen = "com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshiVersion}"
+                const val kotlin = "com.squareup.moshi:moshi-kotlin:${Versions.moshiVersion}"
+            }
+
+            object OkHttp {
+                const val core = "com.squareup.okhttp3:okhttp:${Versions.okhttpVersion}"
+            }
+
+        }
+
+        const val glide = "com.github.bumptech.glide:glide:4.11.0"
+    }
+
+    object Test {
+        object Mockito {
+            const val kotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
+            const val android = "org.mockito:mockito-android:4.4.0"
+        }
+
+        object Kotlin {
+            const val coroutines =
+                "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutineVersion}"
+        }
+
+        const val junit = "junit:junit:4.13.2"
+        const val turbine = "app.cash.turbine:turbine:0.7.0"
+    }
+}
+
