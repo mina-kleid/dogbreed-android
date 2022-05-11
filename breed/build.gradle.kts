47d895d772fd76c0d33c7b49aac90d262ce4ff96
Mina Kleid <minazakiz@gmail.com>
2022-05-11 11:50:10 +0100

    adding breed module

--- breed/build.gradle.kts
+++ breed/build.gradle.kts
@@ -4,31 +4,19 @@ plugins {
     kotlin("kapt")
 }
 
-android {
-    testOptions {
-        unitTests.isReturnDefaultValues = true
-    }
-}
-
 dependencies {
 
-    implementation(project(":breed-item"))
     implementation(project(":common-models"))
     implementation(project(":common-ui"))
-    implementation(project(":network"))
-    implementation(project(":storage"))
 
     implementation(Libraries.App.AndroidX.appcompat)
     implementation(Libraries.App.Kotlin.stdlib)
     implementation(Libraries.App.Lifecycle.viewModel)
     implementation(Libraries.App.Lifecycle.runtime)
+    implementation(Libraries.App.Navigation.fragment)
+    implementation(Libraries.App.Navigation.ui)
+    implementation(Libraries.App.Navigation.dynamicFeatures)
     implementation(Libraries.App.Hilt.core)
 
     kapt(Libraries.App.Hilt.compiler)
-
-    testImplementation(Libraries.Test.Mockito.android)
-    testImplementation(Libraries.Test.Mockito.kotlin)
-    testImplementation(Libraries.Test.Kotlin.coroutines)
-    testImplementation(Libraries.Test.junit)
-    testImplementation(Libraries.Test.turbine)
 }
---
+++ breed/src/main/java/com/mina/dog/breed/BreedViewModel.kt
@@ -0,0 +1,7 @@
+package com.mina.dog.breed
+
+import androidx.lifecycle.ViewModel
+
+class BreedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
