344eceaeaf3219db86912d6c0123aca65ae1f522
Mina Kleid <minazakiz@gmail.com>
2022-06-13 11:59:18 +0800

    fixing holding reference to fragments

--- app/src/main/java/com/mina/dog/breed/ui/main/tabs/Tab.kt
+++ app/src/main/java/com/mina/dog/breed/ui/main/tabs/Tab.kt
@@ -2,4 +2,9 @@ package com.mina.dog.breed.ui.main.tabs
 
 import androidx.fragment.app.Fragment
 
-data class Tab constructor(val title: String, val fragment: Fragment)
+internal data class Tab constructor(val title: String, val tabFragment: TabFragment)
+
+internal enum class TabFragment {
+    BREEDS,
+    NOT_IMPLEMENTED
+}
